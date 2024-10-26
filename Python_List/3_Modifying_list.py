c9cfe6ff2dca850e8e4b759c923b6fe84a419530
mhasanmiju <mehedihasanmiju25@gmail.com>
2024-10-27 00:37:30 +0600

    list 3

---
+++ Python_List/3_Modifying_list.py
@@ -0,0 +1,10 @@
+#Lists in python are mutable, so we can change any values of that list.
+#So we can assign a new value at specific index.
+#Modifying list
+my_list = [1,2,3,4]
+
+#Modifying first element.
+my_list[0] = 10
+
+#printing the modified list
+print(f"Modified list is : {my_list}")
