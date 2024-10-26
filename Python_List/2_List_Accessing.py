eac34a33b113a868abb044b4328aa87bc2aef002
mhasanmiju <mehedihasanmiju25@gmail.com>
2024-10-26 23:50:31 +0600

    List 2

---
+++ Python_List/2_List_Accessing.py
@@ -0,0 +1,14 @@
+#Element in a list are accessed using indices.
+#Indexing start from 0 (First element) but we can start from last element by using -1.
+
+#Example list
+
+my_list = [10,20,30,40,50]
+
+#Accessing elements using positive and negative indices.
+second_element = my_list[1]
+last_element = my_list[-1]
+
+# Printing accessed elements
+
+print(f"Second element is: {second_element}\n Last element is: {last_element} ")
