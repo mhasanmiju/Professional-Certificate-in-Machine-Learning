b838f3c255129ababdb80d1732fb4ec9c7cb50a6
mhasanmiju <mehedihasanmiju25@gmail.com>
2024-10-26 23:06:22 +0600

    list 1

---
+++ Python_List/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+#List is a collection of items in Python, Created using Square Brackets [].
+#In a same list it can contain different data types like numbers,floats,strings,sets,Dictionaries and list as well.
+#Creating a list with various data types
+my_list = [1,2,3,'Hello',[4,5]]
+
+print("Created List: ", my_list)
+print("\n")
+print(f"The list: {my_list}")
