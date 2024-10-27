d2f79c60da98b90840d945c6314cb042ec3b2aa0
mhasanmiju <mehedihasanmiju25@gmail.com>
2024-10-27 15:24:34 +0600

    list 7

---
+++ Python_List/7_list_Comprehension.py
@@ -0,0 +1,6 @@
+# List comprehension is a concise way to create lists using an expression and iteration.
+# It can be used to generate new lists from existing ones.
+# square of numbers from 0 to 4
+squared_list = [x**2 for x in range(5)]
+
+print(f"Squared list is: {squared_list}")
