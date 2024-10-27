83465e7fbf6946ecbb342dc35b9901eb9f7a9e40
mhasanmiju <mehedihasanmiju25@gmail.com>
2024-10-27 16:51:59 +0600

    upadated list 4

--- Python_List/4_Adding_elements.py
+++ Python_List/4_Adding_elements.py
@@ -1,12 +1,15 @@
 # We can add elements to a list by using append() or insert() method
-# append() adds an element to the end and insert() adds an element to a specific index.
-
 my_list = [1,2,3]
 
-#adding an element
+# append() adds an element to the end.
 my_list.append(4)
+
+# Extend list with multiple elements
+my_list.extend([7, 8])
+print(f"1st Updated list: {my_list}")
+# insert() adds an element to a specific index.
 my_list.insert(1,'Inserted')
-print(f"Updated list: {my_list}")
+print(f"2nd Updated list: {my_list}")
 my_list.insert(2, 2.5)
 
 #printing updated list
