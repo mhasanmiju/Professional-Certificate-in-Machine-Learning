1e24ccc7b284835b74661a57c421eaf7422dae65
mhasanmiju <mehedihasanmiju25@gmail.com>
2024-10-27 16:37:36 +0600

    list 10

---
+++ Python_List/10_list_length.py
@@ -0,0 +1,7 @@
+# len() function is used to find the number of elements in a list.
+my_list = [1,2,3,4,5,6]
+
+# Getting the length of the list
+list_lengt = len(my_list)
+
+print(f"The list length is: {list_lengt}")
