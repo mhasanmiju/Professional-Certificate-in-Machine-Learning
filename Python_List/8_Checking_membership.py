41d53ce4ce692f3e75e29065e5189160c7928747
mhasanmiju <mehedihasanmiju25@gmail.com>
2024-10-27 15:46:31 +0600

    list 8

---
+++ Python_List/8_Checking_membership.py
@@ -0,0 +1,8 @@
+# we can use the 'in' keyword to check if an element exists in a list.
+
+my_list = ['Apple','Banana','Cherry']
+
+if 'Banana' in my_list:
+    print(f"Banana is in the List")
+else:
+    print(f"Banana is not in the List")
